d8ccb2b437c1032fd4ca0e0b08ae607a1b3dd3ae
fyang1024 <fyang1024@gmail.com>
2019-01-29 14:06:17 +1100

    added routes definitions

--- src/scripts/router.tsx
+++ src/scripts/router.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {HashRouter, Route, Switch} from 'react-router-dom'
 import Welcome from '../components/welcome/Welcome'
+import { DEFAULT } from './routes'
 
 export default function Router() {
   return <HashRouter hashType="noslash">
     <Switch>
-      <Route exact path="/" component={Welcome}/>
+      <Route exact path={DEFAULT} component={Welcome}/>
     </Switch>
   </HashRouter>
 }
---
+++ src/scripts/routes.ts
@@ -0,0 +1,43 @@
+const DEFAULT = '/'
+const UNLOCK = '/unlock'
+const SETTINGS = '/settings'
+const INFO = '/settings/info'
+const REVEAL_SEED = '/seed'
+const CONFIRM_SEED = '/confirm-seed'
+const RESTORE_VAULT = '/restore-vault'
+const NEW_ACCOUNT = '/new-account'
+const IMPORT_ACCOUNT = '/new-account/import'
+const SEND = '/send'
+const NOTICE = '/notice'
+const WELCOME = '/welcome'
+const INITIALIZE = '/initialize'
+const INITIALIZE_CREATE_PASSWORD = '/initialize/create-password'
+const INITIALIZE_IMPORT_ACCOUNT = '/initialize/import-account'
+const INITIALIZE_IMPORT_WITH_SEED_PHRASE = '/initialize/import-with-seed-phrase'
+const INITIALIZE_UNIQUE_IMAGE = '/initialize/unique-image'
+const INITIALIZE_NOTICE = '/initialize/notice'
+const INITIALIZE_BACKUP_PHRASE = '/initialize/backup-phrase'
+const INITIALIZE_CONFIRM_SEED = '/initialize/confirm-phrase'
+
+export {
+  DEFAULT,
+  UNLOCK,
+  SETTINGS,
+  INFO,
+  REVEAL_SEED,
+  CONFIRM_SEED,
+  RESTORE_VAULT,
+  NEW_ACCOUNT,
+  IMPORT_ACCOUNT,
+  SEND,
+  NOTICE,
+  WELCOME,
+  INITIALIZE,
+  INITIALIZE_CREATE_PASSWORD,
+  INITIALIZE_IMPORT_ACCOUNT,
+  INITIALIZE_IMPORT_WITH_SEED_PHRASE,
+  INITIALIZE_UNIQUE_IMAGE,
+  INITIALIZE_NOTICE,
+  INITIALIZE_BACKUP_PHRASE,
+  INITIALIZE_CONFIRM_SEED
+}
