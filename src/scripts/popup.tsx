e2755d4bb45b08e8f57dcaf1267bd57315500b7d
fyang1024 <fyang1024@gmail.com>
2019-01-31 11:33:02 +1100

    removed dependency on @polkadot/dev-react as yarn detected several moderate vulnerabilities in its dependencies

--- src/scripts/popup.tsx
+++ src/scripts/popup.tsx
@@ -1,22 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider as ReduxProvider } from 'react-redux'
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import { install, ThemeProvider, useTheme } from "@material-ui/styles"
 import Router from './router'
 import configureStore from '../state/store';
 
-const muiTheme = createMuiTheme({
-  typography: {
-    useNextVariants: true,
-  },
-})
+install();
 
 function App(){
   return <ReduxProvider store={configureStore()}>
-      <MuiThemeProvider theme={muiTheme}>
+      <ThemeProvider theme={useTheme()}>
         <Router />
-      </MuiThemeProvider>
+      </ThemeProvider>
   </ReduxProvider>;
 }
 
